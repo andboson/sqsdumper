426f1f07f52cb429e21de186899b51e409669d31
Andrii Bosonchenko <andboson@gmail.com>
2022-09-18 21:26:19 +0200

    Development (#7)
    * updated README, releases
    
    * updated logging
    
    * Added "-stopAfter" and "-raw" arguments
    
    * fixed raw
    
    * refactor

--- internal/commands/dump_messages.go
+++ internal/commands/dump_messages.go
@@ -73,15 +73,13 @@ func (p *SQSDumper) processMessage(_ context.Context, msg types.Message) error {
 		return errors.Wrap(err, "error parsing the incoming message")
 	}
 
-	stringed := *msg.Body
-	if msg.Body == nil {
-		stringed = string(*eventMessage.Message)
-	}
 
 	if p.rawMessage {
-		fmt.Println(stringed)
+		fmt.Println(*msg.Body)
 		return nil
 	}
+
+	stringed := string(*eventMessage.Message)
 
 	if p.jsonPath != "" {
 		return p.printByPath(stringed)
